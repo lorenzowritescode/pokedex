9403c9377c2b337515e4062e93ff3c707c3e0f64
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-01-31 16:05:12 +0000

    Added utility serice

---
+++ app/scripts/services/utility.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name pokedexApp.utility
+ * @description
+ * # utility
+ * Service in the pokedexApp.
+ */
+angular.module('pokedexApp')
+    .service('utility', ['$location', function ($location) {
+        // Lord forgive me for this function
+        this.extractID = function (resourceURI) {
+            var removeTrailingSlash = resourceURI.substring(0, resourceURI.length - 1);
+            var breakpoint = removeTrailingSlash.lastIndexOf('/');
+            return removeTrailingSlash.substring(breakpoint + 1);
+        }
+        
+        this.changeView = function (resourceURI) {
+            $location.path('poke/' + this.extractID(resourceURI));
+        };
+    }]);
